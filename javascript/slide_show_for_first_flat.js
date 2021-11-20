8dc431a06104a6d0a61f8f2857e341abc68747e9
Ekaterina <cherednichenko_kate@mail.ru>
2021-11-20 19:59:59 +0300

    added media queries and phone button event listener

--- javascript/slide_show_for_first_flat.js
+++ javascript/slide_show_for_first_flat.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,9 +12,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {

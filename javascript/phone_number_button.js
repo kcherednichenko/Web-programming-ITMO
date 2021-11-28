1466720c637542b256247464a4830048348c8947
Ekaterina <cherednichenko_kate@mail.ru>
2021-11-28 14:50:12 +0300

    added call by phone number

--- javascript/phone_number_button.js
+++ javascript/phone_number_button.js
@@ -1,14 +1,16 @@
-function showPhoneNumber(phoneNumber, classId) {
+function showPhoneNumber(text, classId, phoneNumber) {
     let button = document.getElementById(classId)
     if (button !== null) {
-        button.addEventListener('click', function (event) {
+        button.addEventListener('click', function myListener(event) {
             event.preventDefault()
-            button.innerHTML = phoneNumber
+            button.innerHTML = text
+            button.setAttribute('href', phoneNumber)
+            button.removeEventListener('click', myListener)
         })
     }
 }
 
-showPhoneNumber("+7(921)100-00-01", "phone_number_1")
-showPhoneNumber("+7(921)100-00-02", "phone_number_2")
-showPhoneNumber("Показать телефон +7(921)100-00-03", "phone_number_3")
-showPhoneNumber("Показать телефон +7(921)100-00-03", "phone_number_4")
+showPhoneNumber("+7(921)100-00-01", "phone_number_1", "tel:+7(921)100-00-01")
+showPhoneNumber("+7(921)100-00-02", "phone_number_2", "tel:+7(921)100-00-02")
+showPhoneNumber("Показать телефон +7(921)100-00-03", "phone_number_3", "tel:+7(921)100-00-03")
+showPhoneNumber("Показать телефон +7(921)100-00-03", "phone_number_4", "tel:+7(921)100-00-03")
